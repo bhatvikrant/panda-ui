48405c913c81bbbc0de16b6a1f35496663bacdbf
Vikrant Bhat <vik.bhat223@gmail.com>
2022-02-06 11:59:59 +0530

    trying to fix exports : (

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
-import Button from "./Button";
-import Avatar from "./Avatar";
+export * as Button from "./Button";
+export * as Avatar from "./Avatar";
 
-export { Button, Avatar };
+// export { Button, Avatar };
 
 // export { default as Button } from "./Button";
 // export { default as Avatar } from "./Avatar";
